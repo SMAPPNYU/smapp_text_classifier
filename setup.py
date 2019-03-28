3d667ba4104d70c07aebe0e243b64f4becb57996
Fridolin Linder <fridolin.linder@nyu.edu>
2019-03-28 11:03:43 -0400

    package setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="smapp_text_classifier",
+    version="0.0.1.9000",
+    author="Fridolin Linder, Michael Liu",
+    author_email="fridolin.linder@nyu.edu",
+    description="Helperfunctions for text classification for SMaPP@NYU.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/smappnyu/smapp_text_classifier",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
--- smapp_text_classifier/__init__.py
+++ smapp_text_classifier/__init__.py
@@ -1,2 +1,4 @@
 import logging
+
+name = 'smapp_text_classifier'
 logging.getLogger(__name__).addHandler(logging.NullHandler())
